ad490710292e2b9681e1a718a2b3cea10450f931
yanarn <nagisa87@outlook.jp>
2024-04-25 00:05:54 +0900

    アスペクト比を変更可

--- script.js
+++ script.js
@@ -2,11 +2,14 @@
 const inch = document.getElementById("screen-size");
 const re_w = document.getElementById("cm_w");
 const re_h = document.getElementById("cm_h");
+const con_aspect_w = document.getElementById("aspect_w");
+const con_aspect_h = document.getElementById("aspect_h");
 
+// 画面サイズから縦横の大きさを計算
 function calculate() {
     // アスペクト比を設定
-    const aspect_w = 16;
-    const aspect_h = 9;
+    const aspect_w = con_aspect_w.value;
+    const aspect_h = con_aspect_h.value;
     const aspect_d = Math.sqrt(aspect_w ** 2 + aspect_h ** 2);
 
     // 計算
@@ -17,4 +20,8 @@ function calculate() {
     // 小数点以下2桁で表示
     re_w.textContent = cm_w.toFixed(2);
     re_h.textContent = cm_h.toFixed(2);
+}
+
+function setAspect() {
+
 }
